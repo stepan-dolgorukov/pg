3611b046f6608254cfb53d75f5fae5202f36579d
Stepan Dolgorukov <dolgorukovpro@gmail.com>
2021-01-29 18:11:05 -0800

    upd resource.h

--- resource.h
+++ resource.h
@@ -1,21 +1,17 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by resource.rc
-//
-#define IDB_ABOUT_ICON                  101
-#define IDB_CLOSE_BUTTON                102
-#define IDB_MINIMIZE_BUTTON             103
-#define IDI_ICON1                       104
-#define APP_ICON_LARGE                  104
-#define APP_ICON                        105
+#pragma once
 
-// Next default values for new objects
-// 
+#define IDB_ABOUT_ICON      101
+#define IDB_CLOSE_BUTTON    102
+#define IDB_MINIMIZE_BUTTON 103
+#define IDI_ICON1           104
+#define APP_ICON_LARGE      104
+#define APP_ICON            105
+
 #ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
+    #ifndef APSTUDIO_READONLY_SYMBOLS
+        #define _APS_NEXT_RESOURCE_VALUE        106
+        #define _APS_NEXT_COMMAND_VALUE         40001
+        #define _APS_NEXT_CONTROL_VALUE         1001
+        #define _APS_NEXT_SYMED_VALUE           101
+    #endif
 #endif
